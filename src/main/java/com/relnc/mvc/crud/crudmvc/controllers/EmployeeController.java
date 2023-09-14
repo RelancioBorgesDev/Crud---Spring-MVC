213e401b6bf897be9bd8498605dae7afe7aa2c7f
Relancio Borges <relancio.dev@gmail.com>
2023-09-14 11:28:44 -0300

    feat: adicionado get dos employees e template para exibir os employees

--- src/main/java/com/relnc/mvc/crud/crudmvc/controllers/EmployeeController.java
+++ src/main/java/com/relnc/mvc/crud/crudmvc/controllers/EmployeeController.java
@@ -1,16 +1,34 @@
 package com.relnc.mvc.crud.crudmvc.controllers;
 
 import com.relnc.mvc.crud.crudmvc.entity.Employee;
+import com.relnc.mvc.crud.crudmvc.services.EmployeeService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private List<Employee> theEmployees;
+    private EmployeeService employeeService;
 
+    public EmployeeController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+
+    }
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+
+        List<Employee> theEmployees = employeeService.findAll();
+
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
 
 }
