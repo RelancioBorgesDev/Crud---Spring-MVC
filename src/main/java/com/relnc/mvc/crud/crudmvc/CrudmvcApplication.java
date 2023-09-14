83b601c4bac293ef075c6699b6b7e41b716083e1
Relancio Borges <relancio.dev@gmail.com>
2023-09-14 09:55:07 -0300

    feat: adicionado projeto crudmvc

---
+++ src/main/java/com/relnc/mvc/crud/crudmvc/CrudmvcApplication.java
@@ -0,0 +1,13 @@
+package com.relnc.mvc.crud.crudmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudmvcApplication.class, args);
+	}
+
+}
