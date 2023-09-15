4aebb48fc3c09c7bb05bed1eb093c44a2987477a
Relancio Borges <relancio.dev@gmail.com>
2023-09-15 12:29:59 -0300

    feat: adicionado metodos de adicionar, atualizar e deletar

--- src/main/java/com/relnc/mvc/crud/crudmvc/controllers/EmployeeController.java
+++ src/main/java/com/relnc/mvc/crud/crudmvc/controllers/EmployeeController.java
@@ -5,8 +5,7 @@ import com.relnc.mvc.crud.crudmvc.services.EmployeeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +27,41 @@ public class EmployeeController {
 
         theModel.addAttribute("employees", theEmployees);
 
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        employeeService.save(employee);
+
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormUpdate")
+    public String showFormUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        Employee theEmployee = employeeService.findById(theId);
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormDelete")
+    public String showFormDelete(@RequestParam("employeeId") int theId, Model theModel) {
+        employeeService.deleteById(theId);
+
+        return "redirect:/employees/list";
     }
 
 }
--- src/main/java/com/relnc/mvc/crud/crudmvc/repositories/EmployeeRepository.java
+++ src/main/java/com/relnc/mvc/crud/crudmvc/repositories/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.relnc.mvc.crud.crudmvc.repositories;
 import com.relnc.mvc.crud.crudmvc.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
